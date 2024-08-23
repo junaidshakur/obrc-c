92db305db26f9fa3f7399d93344d024617490d3b
Junaid <junaid.shakur.js@gmail.com>
2024-08-23 15:46:48 +0500

    use short instead of double for measurement
    measurements in short instead of double to reduce time as we can get same result.

--- obrc.c
+++ obrc.c
@@ -4,20 +4,53 @@
 #include <float.h>
 #include <math.h>
 #include <pthread.h>
+#include <stdbool.h>
+
 
 #define TOTAL_THREADS 6
+#define MIN_TEMP -200
+#define MAX_TEMP 200
 
 typedef struct weatherEntry {
   int id;
-  double min;
-  double max;
+  short min;
+  short max;
   long int rc;
-  double sum;
+  long int sum;
   long int fileCursorStart;
   long int fileCursorEnd;
 } weather;
 
+short get_short_measurement(char *str, int s, int z){
+  short val = 0;
+  bool ng = 0;
+  int i = 0;
+  char c1, c2;
 
+  if (str[i] == '-'){
+          ng = 1;
+          i++;
+  }
+  c1 = str[i++];
+  c2 = str[i++];
+
+  if(c2 == '.'){
+    val += (c1-48)*10;
+  } else {
+    val += (c1-48)*100;
+    val += (c2-48)*10;
+    i++;
+  }
+  val += (str[i]-48);
+
+  if (ng)
+    val *= -1;
+
+ // if (z < 10)
+//        printf("measure final val %d\n", val);
+
+  return val;
+}
 
 void readFile_by_full_line(FILE *fp, weather *pData) {
 
@@ -27,11 +60,11 @@ void readFile_by_full_line(FILE *fp, weather *pData) {
 
   int z = 0;
   long int rc = 0;
-  double sum = 0.0;
+  long int sum = 0;
   char *station;
   char *measurementStr;
-  double measurement;
-  double minMeas = DBL_MAX, maxMeas = DBL_MIN;
+  short measurement;
+  short minMeas = MAX_TEMP, maxMeas = MIN_TEMP;
   char *tokPtr;
   //char *measurementI1, *measurementI2;
   getline(&line, &len, fp);//to move to next stable line instead of reading between the line.
@@ -45,8 +78,9 @@ void readFile_by_full_line(FILE *fp, weather *pData) {
     tokPtr=NULL;
     station = strtok_r(line, ";", &tokPtr);
     measurementStr = strtok_r(NULL, "\n", &tokPtr);
-    measurement = strtod(measurementStr, NULL);
+    //measurement = strtod(measurementStr, NULL);
 
+    measurement = get_short_measurement(measurementStr, sizeof(measurementStr), z);
     // //measurementI1 = (strtok(NULL, "."));
     // //measurementI2 = (strtok(NULL, "\n"));
     sum += measurement;
@@ -56,7 +90,7 @@ void readFile_by_full_line(FILE *fp, weather *pData) {
       minMeas = measurement;
     rc++;
     //if (z < 10)
-    //  printf("station and measurement :: %s:%s\n", station, measurementStr);
+    //  printf("station and measurementStr, measurement :: %s:%s:%d\n", station, measurementStr, measurement);
   }
 
   if (line)
@@ -69,40 +103,6 @@ void readFile_by_full_line(FILE *fp, weather *pData) {
   return;
 }
 
-int readFile_by_delimeter(FILE *fp) {
-  char *line = NULL;
-  size_t len = 0;
-  ssize_t read;
-
-  int z = 0;
-  long long int cc = 0;
-
-  char *station;
-  char *measurementStr;
-  double measurement;
-
-   while ((read = getdelim(&line, &len, ';', fp)) > 0){
-    if (z < 10) z++;
-    cc += read;
-
-    station = strtok(line, ";");
-
-    read = getline(&line, &len, fp);
-    measurementStr = strtok(line, "\n");
-    measurement = atof(measurementStr);
-    cc += read;
-
-     if (z < 10)
-       printf("station and measurement :: %s:%f\n", station, measurement);
-
-    //split text by ; and then first part will be station and seocnd will be measurement
-  }
-
-  if (line)
-    free(line);
-
-  return cc;
-}
 
 void *read_file_chunk(void *args) {
 
@@ -136,9 +136,9 @@ int main() {
   for(int i = 0; i < TOTAL_THREADS; i++){
     pData[i].id = i;
     pData[i].rc = 0;
-    pData[i].min = 0.0;
-    pData[i].max = 0.0;
-    pData[i].sum = 0.0;
+    pData[i].min = MAX_TEMP;
+    pData[i].max = MIN_TEMP;
+    pData[i].sum = 0;
 
     pthread_create(&thread[i], NULL, read_file_chunk, &pData[i]);
   }
@@ -147,12 +147,12 @@ int main() {
     pthread_join(thread[i], NULL);
   }
 
-  long int rc=0;
-  double min=DBL_MAX, max=DBL_MIN, sum = 0;
+  long int rc=0, sum = 0;
+  short min=MAX_TEMP, max=MIN_TEMP;
 
   for(int i = 0; i < TOTAL_THREADS; i++){
     printf("File cursors of threads %i are %ld, %ld\n", i, pData[i].fileCursorStart, pData[i].fileCursorEnd);
-    printf("%d:: Records:%ld:: Sum:%f:: MIN:%f:: MAX:%f\n", pData[i].id, pData[i].rc, pData[i].sum, pData[i].min, pData[i].max);
+    printf("%d:: Records:%ld:: Sum:%ld:: MIN:%d:: MAX:%d\n", pData[i].id, pData[i].rc, pData[i].sum, pData[i].min, pData[i].max);
     rc += pData[i].rc;
     sum += pData[i].sum;
     if (pData[i].min < min)
@@ -161,7 +161,7 @@ int main() {
       max = pData[i].max;
   }
 
-  printf("\n\nTotal record count, sum, avg, min and max %ld:%f:%f:%f:%f\n", rc, sum, sum/(double)rc, min, max);
+  printf("\n\nTotal record count, sum, avg, min and max %ld:%ld:%f:%f:%f\n", rc, sum, (sum/(double)rc)/10.0, min/10.0, max/10.0);
 
 
   exit(EXIT_SUCCESS);
